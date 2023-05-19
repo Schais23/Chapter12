9c14770c063d2ce14a98958cf257bc87041e8672
Schais23 <stefann23si@gmail.com>
2023-05-20 05:53:23 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -46,19 +46,12 @@ def home():
         )
         user_info = db.users.find_one({"username": payload.get("id")})
         return render_template('index.html', user_info=user_info)
-    # except jwt.ExpiredSignatureError:
-    #     msg = 'Your token has expired'
-    #     return redirect(url_for("login", msg=msg ))
-    # except jwt.exceptions.DecodeError:
-    #     msg = 'There was problem logging you in'
-    #     return redirect(url_for("login", msg=msg))
-
     except jwt.ExpiredSignatureError:
         msg = 'Your token has expired'
-        return render_template("index.html", msg=msg)
+        return redirect(url_for("login", msg=msg ))
     except jwt.exceptions.DecodeError:
-        msg = 'There was a problem logging you in'
-        return render_template("index.html", msg=msg)
+        msg = 'There was problem logging you in'
+        return redirect(url_for("login", msg=msg))
 
 
 @app.route("/login", methods= ['GET'])
